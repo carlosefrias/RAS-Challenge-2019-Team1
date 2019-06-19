098d11b89bb1fb190827cf7db415f6eff5b47643
Carlos Frias <carlosefrias@gmail.com>
2019-06-19 16:28:24 +0100

    template matching working

--- opencv-tool-detection/template-matching.py
+++ opencv-tool-detection/template-matching.py
@@ -2,14 +2,15 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('capture.png',0)
+img = cv2.imread('capture3.png', 0)
 img2 = img.copy()
-template = cv2.imread('hammer.jpg',0)
+template = cv2.imread('hammer.png', 0)
 w, h = template.shape[::-1]
 
 # All the 6 methods for comparison in a list
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+# methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+#             'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+methods = ['cv2.TM_CCOEFF']
 
 for meth in methods:
     img = img2.copy()
@@ -25,12 +26,14 @@ for meth in methods:
     else:
         top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
+    print(f"top_left: {top_left}\nbottom_right: {bottom_right}")
+    
 
     cv2.rectangle(img,top_left, bottom_right, 255, 2)
 
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.subplot(121),plt.imshow(res)
     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.subplot(122),plt.imshow(img)
     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
     plt.suptitle(meth)
 
